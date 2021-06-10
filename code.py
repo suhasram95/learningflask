554fc9017bf3c0676ac03bc32b1015e02becc732
Suhas Ramesh <suhasram95@gmail.com>
2021-06-10 07:35:13 -0500

    Added file code.py

--- code.py
+++ code.py
@@ -1,15 +1,21 @@
 from statistics import median
 class Student:
-    def __init__(self, name):
+    def __init__(self, name, grades):
         self.name = name
-        self.grades = (90, 90, 93, 78, 90)
+        self.grades = grades
     def average_grade(self):
         return sum(self.grades) / len(self.grades)
     def student_median(self):
         return median(self.grades)
 
-student = Student("Bob")
+student = Student("Bob", (90, 90, 93, 78, 90))
+student2 = Student("Rolf", (90, 90, 93, 78, 90))
 print (student.name)
 print (student.grades)
 print (student.average_grade())
 print (student.student_median())
+print ("=============================")
+print (student2.name)
+print (student2.grades)
+print (student2.average_grade())
+print (student2.student_median())
