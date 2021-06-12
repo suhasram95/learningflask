fd2198db4fa0e4a5453ded86ceaaf966c6f4ed5c
Suhas Ramesh <suhasram95@gmail.com>
2021-06-12 09:11:11 -0500

    Added requirements.txt file

--- app.py
+++ app.py
@@ -4,7 +4,13 @@ app = Flask(__name__)
 
 hello = "Hello, world! This is from the program I am using flask"
 @app.route('/store', methods=['POST', 'GET'])
-def home():
-    return hello
+def create_store():
+    pass
+
+@app.route('/store/<string:name>', methods=['GET'])
+def get_store():
+    pass
+
+def 
 
 app.run(port=5000)
