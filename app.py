f2ff8cf15bb0278838ce6ed2cffb45ae786911be
Suhas Ramesh <suhasram95@gmail.com>
2021-06-12 09:54:08 -0500

    Modified file app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import os
 app = Flask(__name__)
 stores =[
@@ -14,7 +14,13 @@ stores =[
 ]
 @app.route('/store', methods=['POST'])
 def create_store():
-    pass
+    request_data = request.get_json()
+    new_store = {
+        'name': request_data['name'],
+        'items': []
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
 
 @app.route('/store')
 def get_stores():
