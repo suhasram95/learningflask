54f57bade2dad75d3063565ed1a016ef5a7708be
Suhas Ramesh <suhasram95@gmail.com>
2021-06-12 19:03:44 -0500

    Edited file app.py

--- app.py
+++ app.py
@@ -8,10 +8,8 @@ items = []
 
 class Item(Resource):
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-        return {'item': None}, 404
+        item = next(filter(lambda x : x['name'] == name, items), None)
+        return {'item': None}, 200 if item is not None else 404
 
     def post(self, name):
         data = request.get_json(force = True)
