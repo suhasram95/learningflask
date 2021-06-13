75e215e1454054062c36b97c7026d0e1c360ec13
Suhas Ramesh <suhasram95@gmail.com>
2021-06-12 22:03:27 -0500

    Deleted the utility file

--- gitutility.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# This utility will push help in pushing the changes from the current branch to the repo
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
-MESSAGE=$1
-git add .
-git commit -m "$MESSAGE"
-git push origin $BRANCH
