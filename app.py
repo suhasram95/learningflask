c7a08cb828ad0a545218dec2f3ea246c78c10ed5
Suhas Ramesh <suhasram95@gmail.com>
2021-06-12 21:22:46 -0500

    Edited files

--- app.py
+++ app.py
@@ -12,6 +12,17 @@ jwt = JWT(app, authenticate, identity)
 items = []
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('name',
+        type = str,
+        required = True,
+        help = "Name Cannot be blank!")
+    parser.add_argument(
+        'price',
+        type = float,
+        required = True,
+        help = "Price Cannot be  blank!"
+    )
     def get(self, name):
         item = next(filter(lambda x : x['name'] == name, items), None)
         return {'item': None}, 200 if item is not None else 404
@@ -19,7 +30,7 @@ class Item(Resource):
     def post(self, name):
         if next(filter(lambda x : x['name'] == name, items), None) is not None:
             return {'message': "An item with name '{}' already exists.".format(name)}, 409
-        data = request.get_json(force = True)
+        data = Item.parser.parse_args()
         item = {'name': data['name'], 'price': data['price']}
         items.append(item)
         return item, 201
@@ -30,7 +41,7 @@ class Item(Resource):
         return {'message': 'Item deleted'}, 204
     
     def put(self, name):
-        data = request.get_json(force = True)
+        data = Item.parser.parse_args()
         item = next(filter(lambda x : x['name'] == name, items), None)
         if item is None:
             item = {'name': name, 'price': data['price']}
