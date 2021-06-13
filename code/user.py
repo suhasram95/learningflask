6928bfff829631d5f3ced9c99cb6ab76338b126d
Suhas Ramesh <suhasram95@gmail.com>
2021-06-12 22:02:15 -0500

    Added files

--- code/user.py
+++ code/user.py
@@ -1,6 +1,13 @@
+import sqlite3
 class User:
     def __init__(self, _id, username, password):
         self.id = _id
         self.username = username
         self.password = password
         
+    def find_by_username(self, username):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM users WHERE username=?"
+        result = cursor.execute(query, (username,))
