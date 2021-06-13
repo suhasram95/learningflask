6697d34c802b57689f4490ed77e1aa5cf58f0747
Suhas Ramesh <suhasram95@gmail.com>
2021-06-12 20:53:45 -0500

    Added files

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
@@ -12,7 +12,6 @@ jwt = JWT(app, authenticate, identity)
 items = []
 
 class Item(Resource):
-    @jwt_required()
     def get(self, name):
         item = next(filter(lambda x : x['name'] == name, items), None)
         return {'item': None}, 200 if item is not None else 404
@@ -24,6 +23,22 @@ class Item(Resource):
         item = {'name': data['name'], 'price': data['price']}
         items.append(item)
         return item, 201
+    
+    def delete(self, name):
+        global items 
+        items = list(filter(lambda x : x['name'] != name, items))
+        return {'message': 'Item deleted'}, 204
+    
+    def put(self, name):
+        data = request.get_json(force = True)
+        item = next(filter(lambda x : x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
+
 
 class ItemList(Resource):
     def get(self):
