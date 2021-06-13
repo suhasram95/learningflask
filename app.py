b7e2976d7d74ddf2b1ee139a944eda34331827cb
Suhas Ramesh <suhasram95@gmail.com>
2021-06-12 19:40:53 -0500

    Edited file app.py

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = 'ramesh'
 api = Api(app)
 
+jwt = JWT(app, authenticate, identity)
 items = []
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         item = next(filter(lambda x : x['name'] == name, items), None)
         return {'item': None}, 200 if item is not None else 404
 
     def post(self, name):
+        if next(filter(lambda x : x['name'] == name, items), None) is not None:
+            return {'message': "An item with name '{}' already exists.".format(name)}, 409
         data = request.get_json(force = True)
         item = {'name': data['name'], 'price': data['price']}
         items.append(item)
